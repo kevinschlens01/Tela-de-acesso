8470bb2bddd744481ad229677f1a27708368ab0a
Kevin Schlens <116759458+kevinschlens01@users.noreply.github.com>
2022-11-04 16:49:49 -0300

    Add files via upload

---
+++ UserName/UserName/FormLogin.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace UserName
+{
+    partial class FormLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAcesso = new System.Windows.Forms.Button();
+            this.lblUser = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnAcesso
+            // 
+            this.btnAcesso.Location = new System.Drawing.Point(145, 81);
+            this.btnAcesso.Name = "btnAcesso";
+            this.btnAcesso.Size = new System.Drawing.Size(93, 31);
+            this.btnAcesso.TabIndex = 0;
+            this.btnAcesso.Text = "Acesso";
+            this.btnAcesso.UseVisualStyleBackColor = true;
+            this.btnAcesso.Click += new System.EventHandler(this.btnAcesso_Click);
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUser.Location = new System.Drawing.Point(53, 16);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(57, 17);
+            this.lblUser.TabIndex = 1;
+            this.lblUser.Text = "Usuário";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.Location = new System.Drawing.Point(116, 10);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(147, 26);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(116, 49);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(147, 26);
+            this.txtSenha.TabIndex = 4;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPassword.Location = new System.Drawing.Point(53, 55);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(49, 17);
+            this.lblPassword.TabIndex = 3;
+            this.lblPassword.Text = "Senha";
+            // 
+            // FormLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 146);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.lblUser);
+            this.Controls.Add(this.btnAcesso);
+            this.Name = "FormLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FormLogin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAcesso;
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lblPassword;
+    }
+}
---
+++ UserName/UserName/FormLogin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UserName
+{
+    public partial class FormLogin : Form
+    {
+        public FormLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAcesso_Click(object sender, EventArgs e)
+        {
+            string user = "admin";
+            string password = "admin";
+
+            if (txtUsuario.Text == user & txtSenha.Text == password)
+            {
+                MessageBox.Show("Acesso Liberado");
+                Form1 FormMain = new Form1();
+                FormMain.Show();
+                this.Hide();
+            }else
+            {
+                MessageBox.Show("Usuário/Senha incorretos");
+            }
+        }
+    }
+}
